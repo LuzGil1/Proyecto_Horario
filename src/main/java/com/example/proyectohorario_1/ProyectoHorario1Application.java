cc8eb54d2597916e7eecc9a9910026de8767a397
LuzmiG <luzgil2002@gmail.com>
2025-10-06 19:55:01 -0600

    modificacion de properties

--- src/main/java/com/example/proyectohorario_1/ProyectoHorario1Application.java
+++ src/main/java/com/example/proyectohorario_1/ProyectoHorario1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class    ProyectoHorario1Application {
-//    hi!
+//    hiiii!
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoHorario1Application.class, args);
